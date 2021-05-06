a7de2db6cf9391a384b4ea22332e6856c403ea01
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-05-06 11:39:38 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+// Public libraries
+const express = require("express");
+// Personal libraries
+// Initialize app environment
+const app = express();
+// Elements from env
+require("dotenv").config();
+// Variables
+const PORT = 3000;
