4b0f3a8f16ff2bc52182a52664f1f77606ebbc00
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-05-06 12:42:38 -0400

    Created a model and testing with database

---
+++ backend/models/user.js
@@ -0,0 +1,56 @@
+// public libraries
+const mongoose = require("mongoose");
+// private libraries
+// Create Schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 20,
+    },
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    hash_password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    contactNumber: {
+      type: String,
+    },
+    profilePicture: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Export elements
+module.exports = mongoose.model("User", userSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,48 @@
 // public libraries
 const express = require("express");
 // private libraries
+const User = require("../models/user");
 // Initiallize express routes
 const router = express.Router();
 // Routes get
 // Routes post
 router.post("/signin", (req, res) => {});
-router.post("/signup", (req, res) => {});
+router.post("/signup", (req, res) => {
+  // Destructure elements of sigin
+  const { firstName, lastName, email, userName, password } = req.body;
+  // Check if user is already register
+  User.findOne({ email: email })
+    .then((user) => {
+      // If user exist, then we send an error, we cannot have two users
+      if (user) {
+        return res.status(422).json({
+          error: "User already exist, try another email",
+        });
+      }
+      // Else, user is not registe, save parameters
+      const _user = new User({
+        firstName,
+        lastName,
+        email,
+        userName,
+        hash_password: password,
+      });
+      // now save parameters and display results
+      _user
+        .save()
+        .then((data) => {
+          return res.status(200).json({
+            user: data,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 // Routes put
 // Routes delete
 // export modules
