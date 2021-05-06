91c3d06a5a98379defec799cc687200b83935cdb
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-05-06 12:08:38 -0400

    Set up server.js and routes with user elements

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+// public libraries
+const express = require("express");
+// private libraries
+// Initiallize express routes
+const router = express.Router();
+// Routes get
+// Routes post
+router.post("/signin", (req, res) => {});
+router.post("/signup", (req, res) => {});
+// Routes put
+// Routes delete
+// export modules
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,38 @@
 // Public libraries
 const express = require("express");
+const mongoose = require("mongoose");
 // Personal libraries
 // Initialize app environment
 const app = express();
 // Elements from env
 require("dotenv").config();
 // Variables
-const PORT = 3000;
+const port = process.env.PORT || 5000;
+const db = process.env.MONGOURI;
+// Variables and links from files
+const userRoutes = require("./routes/user");
+// MongoDb database
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
+// Check if connected
+mongoose.connection
+  .on("error", (err) => {
+    console.error(err);
+  })
+  .on("connected", (err) => {
+    console.log(`DB connected on ${db}`);
+  });
+// Middlewares
+// JSON Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// user Routes
+app.use("/api", userRoutes);
+// Listening port
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
